f924ce22ffc9559eb91e7ead1f8c95a95ae3343f
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2024-07-14 21:00:34 +0300

    .

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Address> Addresses {  get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; database=MultiShopOrderDb; integrated security=true;");
+        }
+    }
+}
